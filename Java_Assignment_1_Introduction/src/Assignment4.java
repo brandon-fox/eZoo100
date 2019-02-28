2f97961182ccffbf8659c2ecbb0202b7e4793bed
brandon-fox <bfox@foxtrot.email>
2019-02-27 20:01:02 -0500

    Assignment 1 Basics of Java

---
+++ Java_Assignment_1_Introduction/src/Assignment4.java
@@ -0,0 +1,8 @@
+
+public class Assignment4 {
+    public static void main(String[] args){
+        @SuppressWarnings("MismatchedReadAndWriteOfArray") int[] array = new int[5];
+        System.out.println(array.length);
+    }
+
+}
---
+++ Java_Assignment_1_Introduction/src/Assignment9.java
@@ -0,0 +1,8 @@
+
+public class Assignment9 {
+    public static void main(String[] args){
+        boolean var = true;
+        //noinspection ConstantConditions
+        System.out.println(var);
+    }
+}
